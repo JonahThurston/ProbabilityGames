06489e3a33a25bb51b1abc13458d395fd111b824
JonahThurston <jonah.m.thurston@gmail.com>
2025-09-22 12:22:12 -0600

    finish petersburg

--- petersburgParadox.py
+++ petersburgParadox.py
@@ -1,13 +1,36 @@
 import random
 import matplotlib.pyplot as plt
 
-# Generate 20 random numbers between 1 and 100
-data = [random.randint(1, 100) for _ in range(20)]
+SAMPLE_SIZES = [100, 1000, 10000, 100000, 1000000]
 
-# Plot as a histogram
-plt.hist(data, bins=10, color="skyblue", edgecolor="black")
+def runGame():
+  numFlips = 0
 
-plt.title("Histogram of Random Numbers")
-plt.xlabel("Value")
-plt.ylabel("Frequency")
+  while True:
+    numFlips += 1
+    result = random.choice(["HEADS", "TAILS"])
+    if result == "TAILS":
+      break
+
+  winnings = 2 ** numFlips
+  return winnings
+
+averages = []
+
+for size in SAMPLE_SIZES:
+  total = 0
+  for i in range(size):
+    total += runGame()
+  average = total / size
+  averages.append(average)
+  print(f"sample size: {size}    sum: {total}    avg: {average}")
+
+# ngl, I made chat do the plots, because idk matplotlib
+plt.figure(figsize=(8, 5))
+plt.plot(SAMPLE_SIZES, averages, marker="o")
+plt.xscale("log")  # log scale makes sense here
+plt.xlabel("Sample Size (log scale)")
+plt.ylabel("Average Winnings")
+plt.title("St. Petersburg Paradox Simulation")
+plt.grid(True)
 plt.show()
